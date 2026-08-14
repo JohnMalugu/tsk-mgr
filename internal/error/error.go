f288ffa5045aaeb634c2c247b02aeb248d6f5906
JohnMalugu <johnmalugu99@gmail.com>
2026-08-14 07:57:00 +0300

    error response

--- internal/error/error.go
+++ internal/error/error.go
@@ -9,3 +9,12 @@ import (
 	"time"
 )
 
+// ErrorResponse represents a standard error response
+type ErrorResponse struct {
+	Status    int       `json:"status"`
+	Error     string    `json:"error"`
+	Message   string    `json:"message"`
+	Timestamp time.Time `json:"timestamp"`
+	Path      string    `json:"path"`
+	TraceID   string    `json:"traceId,omitempty"`
+}
