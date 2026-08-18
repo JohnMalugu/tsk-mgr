725609e3e62ac83df1603751cc5644a7c45e62d4
JohnMalugu <johnmalugu99@gmail.com>
2026-08-18 09:19:31 +0300

    success with response

--- internal/error/error.go
+++ internal/error/error.go
@@ -50,3 +50,9 @@ func RespondWithError(w http.ResponseWriter, r *http.Request, appErr *AppError) 
 	json.NewEncoder(w).Encode(errorResponse)
 }
 
+// RespondWithSuccess sends a successful response
+func RespondWithSuccess(w http.ResponseWriter, data interface{}, statusCode int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(data)
+}
