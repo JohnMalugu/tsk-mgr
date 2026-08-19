7c305e6f3f30e2832919211a19f99a3dd1b04011
JohnMalugu <johnmalugu99@gmail.com>
2026-08-19 08:00:36 +0300

    validation error

--- internal/validation/task.go
+++ internal/validation/task.go
@@ -8,3 +8,8 @@ import (
 	"github.com/yourusername/task-manager-api/internal/model"
 )
 
+// ValidationError holds validation errors
+type ValidationError struct {
+	Field   string `json:"field"`
+	Message string `json:"message"`
+}
