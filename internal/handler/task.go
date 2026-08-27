58dff0b0168441dbbc9a9b1fbfe7d637825a3455
JohnMalugu <johnmalugu99@gmail.com>
2026-08-28 00:08:52 +0300

    xdfg

--- internal/handler/task.go
+++ internal/handler/task.go
@@ -17,3 +17,9 @@ type Handler struct {
 	taskService *service.TaskService
 }
 
+// NewHandler creates a new Handler
+func NewHandler(taskService *service.TaskService) *Handler {
+	return &Handler{
+		taskService: taskService,
+	}
+}
