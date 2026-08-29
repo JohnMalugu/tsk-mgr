2b96bec82d6531c394256ff589ae3e4e9c613c19
JohnMalugu <johnmalugu99@gmail.com>
2026-08-30 00:02:25 +0300

    sooner tmr

--- internal/handler/task.go
+++ internal/handler/task.go
@@ -36,8 +36,5 @@ func (h *Handler) HandleTasks(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	appErr := &apierror.AppError{
-		Code:    http.StatusMethodNotAllowed,
-		Message: "Method not allowed",
-	}
+	
 }
