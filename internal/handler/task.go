80e8931728a7c07b7956f490fa692276c5dcb7af
JohnMalugu <johnmalugu99@gmail.com>
2026-08-31 21:47:33 +0300

    username

--- internal/handler/task.go
+++ internal/handler/task.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/yourusername/task-manager-api/internal/error"
+	"github.com/john/task-manager-api/internal/error"
 	"github.com/yourusername/task-manager-api/internal/model"
 	"github.com/yourusername/task-manager-api/internal/service"
 	"github.com/yourusername/task-manager-api/internal/validation"
