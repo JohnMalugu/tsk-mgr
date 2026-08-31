7402de795ceeffac83f35953e8deac10a1815404
JohnMalugu <johnmalugu99@gmail.com>
2026-08-31 21:47:56 +0300

    usebame

--- internal/handler/task.go
+++ internal/handler/task.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/john/task-manager-api/internal/error"
 	"github.com/john/task-manager-api/internal/model"
-	"github.com/yourusername/task-manager-api/internal/service"
+	"github.com/john/task-manager-api/internal/service"
 	"github.com/yourusername/task-manager-api/internal/validation"
 )
 
