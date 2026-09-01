1fdc97f549168ae91018c947986f39dcf541ef84
JohnMalugu <johnmalugu99@gmail.com>
2026-09-01 10:04:29 +0300

    name on git link

--- internal/handler/task.go
+++ internal/handler/task.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/john/task-manager-api/internal/error"
+	"github.com/JohnMalugu/task-manager-api/internal/error"
 	"github.com/john/task-manager-api/internal/model"
 	"github.com/john/task-manager-api/internal/service"
 	"github.com/john/task-manager-api/internal/validation"
