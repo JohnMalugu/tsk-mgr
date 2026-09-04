1035dc07b571f363452b24ecad658b64784fd7b3
JohnMalugu <johnmalugu99@gmail.com>
2026-09-04 14:08:39 +0300

    ndc

--- internal/handler/task.go
+++ internal/handler/task.go
@@ -7,11 +7,3 @@ type Handler struct {
 	taskService *service.TaskService
 }
 
-// NewHandler creates a new Handler
-func NewHandler(taskService *service.TaskService) *Handler {
-	return &Handler{
-		taskService: taskService,
-	}
-}
-
-}
