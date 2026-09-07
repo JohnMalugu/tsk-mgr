791cbaaa247bc69c93f8aa1c6c84eb63ff7fd877
JohnMalugu <johnmalugu99@gmail.com>
2026-09-07 17:02:13 +0300

    hf

--- internal/handler/task.go
+++ internal/handler/task.go
@@ -25,9 +25,4 @@ func (h *Handler) HandleTasks(w http.ResponseWriter, r *http.Request) {
 
 
 
-	appErr := &apierror.AppError{
-		Code:    http.StatusMethodNotAllowed,
-		Message: "Method not allowed",
-	}
-	apierror.RespondWithError(w, r, appErr)
 }
