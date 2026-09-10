fe6c2f6075cc919cf2c0fe64d32c7484863c75cf
JohnMalugu <johnmalugu99@gmail.com>
2026-09-10 19:08:15 +0300

    ngg nus

--- internal/handler/task.go
+++ internal/handler/task.go
@@ -1,5 +1,25 @@
 package handler
 
+import (
+	
+)
+
+// Handler holds dependencies for all handlers
 type Handler struct {
 	taskService *service.TaskService
+}
+
+// NewHandler creates a new Handler
+func NewHandler(taskService *service.TaskService) *Handler {
+	return &Handler{
+		taskService: taskService,
+	}
+}
+
+// HandleTasks handles GET and POST requests for /tasks
+func (h *Handler) HandleTasks(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		h.HandleGetAllTasks(w, r)
+		return
+	}
 }
