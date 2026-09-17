b33b77f26cb72ffc7d2825cd587fcbfe006cf28e
JohnMalugu <johnmalugu99@gmail.com>
2026-09-17 22:06:01 +0300

    okpkio

--- internal/handler/task.go
+++ internal/handler/task.go
@@ -72,5 +72,4 @@ func respondJSON(w http.ResponseWriter, status int, value interface{}) {
 }
 
 func respondError(w http.ResponseWriter, r *http.Request, status int, message string) {
-	appError.RespondWithError(w, r, appError.NewAppError(status, message, nil))
-}
+	a
